bc9f6aa7e5edf35897fe5fe7d58eb65c065067c0
robbietree8 <wangzg1988@gmail.com>
2022-03-03 16:51:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# DAC8568(WIP)
+
+## Description
+
+DAC8568是一个8通道、16位精度的数模转换器，兼容`SPI`, `QSPI`, `Microwave`和`DSP`接口。
+
+## Features
+
+1. 给指定通道施加指定的电压
+2. 设置使用内部参考电压
+2. 设置使用外部参考电压
+
+## Example
+
+```python
+from machine import SPI, Pin
+import dac8568
+
+# define spi/sync, esp32-s for example
+spi = SPI(2, 10000)
+sync = machine.Pin(5, Pin.OUT)
+
+# init dac8568
+dac8568_ = dac8568.DAC8568(spi, sync)
+
+# use internal reference
+dac8568_.use_internal_reference()
+
+# set voltage
+dac8568_.set_voltage(dac8568.Channel.A, 65535)
+```
+
+## Reference
+
+- [rust version](https://github.com/ostenning/dac8568)
+- [data sheet](https://www.ti.com.cn/product/cn/DAC8568)
+
---
+++ dac8568.py
@@ -0,0 +1,121 @@
+from micropython import const
+
+class Channel:
+  """Channel Enum."""
+
+  A = const(0x00)
+  B = const(0x01)
+  C = const(0x02)
+  D = const(0x03)
+  E = const(0x04)
+  F = const(0x05)
+  G = const(0x06)
+  H = const(0x07)
+
+class Control:
+  """Control Enum."""
+  WriteToInputRegister = const(0x00)
+  WriteToChannelAndUpdateAllRegisters = const(0x2)
+  WriteToChannelAndUpdateSingleRegister = const(0x03) # 00000011
+  InternalReferenceStatic = const(0x08) # 00001000
+
+class Feature:
+  """Feature Enum."""
+
+  InternalReference = const(0x00)
+  ExternalReference = const(0x01)
+
+class Payload:
+  """Data Input Register Format"""
+
+  def __init__(self):
+    self.prefix = const(0x00) # 4bits
+    self.control = const(0x00) # 4bits
+    self.address = const(0x00) # 4bits
+    self.data = const(0x00) # 16bits
+    self.feature = const(0x00) # 4bits
+  
+  def set_control(self, control):
+    self.control = control
+  
+  def set_address(self, address):
+    self.address = address
+
+  def set_data(self, data):
+    self.data = data
+
+  def set_feature(self, feature):
+    self.feature = feature
+  
+  def to_bytes(self):
+    result = 0
+    result = result | self.prefix << 28
+    result = result | self.control << 24
+    result = result | self.address << 20
+    result = result | self.data << 4
+    result = result | self.feature << 0
+    return result.to_bytes(4, 'big')
+
+class DAC8568:
+  """Implementation for dac8568"""
+
+  def __init__(self, spi, sync):
+    self.spi = spi
+    self.sync = sync
+  
+  def use_internal_reference(self):
+    payload:Payload = Payload()
+    payload.set_control(Control.InternalReferenceStatic)
+    payload.set_feature(Feature.InternalReference)
+    self.__write__(payload)
+
+  def use_external_reference(self):
+    payload:Payload = Payload()
+    payload.set_control(Control.InternalReferenceStatic)
+    payload.set_feature(Feature.ExternalReference)
+    self.__write__(payload)
+
+  """set voltage to specified channel"""
+  def set_voltage(self, channel, voltage):
+    payload:Payload = self.set_voltage0(channel, voltage)
+    payload.set_control(Control.WriteToInputRegister)
+    self.__write__(payload)
+
+  """set voltage to specified channel, and update"""
+  def set_and_update_voltage(self, channel, voltage):
+    payload:Payload = self.set_voltage0(channel, voltage)
+    payload.set_control(Control.WriteToChannelAndUpdateSingleRegister)
+    self.__write__(payload)
+  
+  """set voltage to specified channel, and update all"""
+  def set_and_update_all_voltage(self, channel, voltage):
+    payload:Payload = self.set_voltage0(channel, voltage)
+    payload.set_control(Control.WriteToChannelAndUpdateAllRegisters)
+    self.__write__(payload)
+
+  def set_voltage0(self, channel, voltage):
+    voltage = self.check_voltage(voltage)
+    payload:Payload = Payload()
+    payload.set_address(channel)
+    payload.set_data(voltage)
+    return payload
+  
+  def check_voltage(self, voltage):
+    if voltage < 0:
+      voltage = 0
+    if voltage > 65535:
+      voltage = 65535
+    return voltage
+
+  def __write__(self, payload):
+    self.sync.off()
+    result = self.spi.write(payload.to_bytes())
+    print(result)
+    self.sync.on()
+
+if __name__ == '__main__':
+  payload = Payload()
+  payload.set_control(Control.WriteToChannelAndUpdateSingleRegister)
+  payload.set_address(Channel.A)
+  payload.set_data(65535)
+  print(payload.to_bytes())
